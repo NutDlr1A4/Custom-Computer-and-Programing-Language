d3feab5a23ee1f049d60158343462f76ad85ce6d
NutDealer1000101 <Lukan@DESKTOP-F8C9BG3>
2025-03-06 23:18:53 +0100

    Minor fixes to the NUT16. It runs Hello World now!

---
+++ Assembler/Tests/helloworld.asm
@@ -0,0 +1,16 @@
+; Simple hello world program (hopefully)
+@code	
+.start		sport 1				; Port set to terminal
+			ld r2 helloworld	; r2 points to string
+.loop		ldb r1 r2			; Reads character at pointer
+			cmp r1 '\0'			
+			beq endloop			; If char == '\0', break from loop
+			wport r1			; Write char to terminal
+			add r2 1
+			bra loop			; Loop back
+.endloop	hlt					; Halt program
+
+@data
+.helloworld		
+			"Hello World!\n\0"
+
